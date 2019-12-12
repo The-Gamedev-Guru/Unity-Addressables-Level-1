35b43294866bd8d2ec14c047138f81239c864584
rtorresbonet <rtorresbonet@forcefieldxr.com>
2019-12-12 10:09:01 +0100

    Reupload

---
+++ Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SkyboxManager : MonoBehaviour
+{
+    [SerializeField] private Material[] _skyboxMaterials;
+
+    public void SetSkybox(int skyboxIndex)
+    {
+        RenderSettings.skybox = _skyboxMaterials[skyboxIndex];
+    }
+}
